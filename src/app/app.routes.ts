f1623371e52b1767189c2186a5a776a052c211ae
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-07-23 14:17:29 -0500

    ✨ feat(routes): crea rutas
    se crea el enrutado para las diferentes vistas de los compoentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './components/products/list/list.component';
+import { CreateComponent } from './components/products/create/create.component';
+import { UpdateComponent } from './components/products/update/update.component';
+import { ViewComponent } from './components/products/view/view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ListComponent },
+  { path: 'products/:id', component: ViewComponent },
+  { path: 'products/create', component: CreateComponent },
+  { path: 'products/update/:id', component: UpdateComponent },
+];
